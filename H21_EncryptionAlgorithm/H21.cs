c526594c6b5060f15201fe8330d10d4aae49753b
Hadi Bagheri <153322772+HadiBagheri9@users.noreply.github.com>
2024-07-06 15:38:16 +0330

    Add project files.

---
+++ H21_EncryptionAlgorithm/H21.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H21_EncryptionAlgorithm
+{
+    class H21
+    {
+        public static string Enc(string text)
+        {
+            string str = text;
+            char[] strToCharArray = str.ToCharArray();
+
+            return str;
+        }
+    }
+}
---
+++ H21_EncryptionAlgorithm/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H21_EncryptionAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(H21.Enc(input));
+        }
+    }
+}
